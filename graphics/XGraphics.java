477d0be37c0dfabacb4ebf3efb3c14a0137af72d
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2022-07-15 10:55:29 +0800

    Update XGraphics.java

--- graphics/XGraphics.java
+++ graphics/XGraphics.java
@@ -1,4 +1,10 @@
 package graphics;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.GeneralPath;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+
 public class Graphics {
 }
