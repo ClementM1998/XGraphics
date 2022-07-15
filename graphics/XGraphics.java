e8300a05ab117348c130f2df11fc221d826031bc
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2022-07-15 11:02:06 +0800

    Update XGraphics.java

--- graphics/XGraphics.java
+++ graphics/XGraphics.java
@@ -11,5 +11,12 @@ public class Graphics {
     private static String title = "";
     private static int screenWidth = 640;
     private static int screenHeight = 480;
-    
+    private static int width = screenWidth;
+    private static int height = screenHeight;
+    private static int posx = 0;
+    private static int posy = 0;
+    private static Color color = Color.white;
+    private static Color backColor = Color.black;
+    private static boolean filled = false;
+    private static boolean antialias = true;
 }
