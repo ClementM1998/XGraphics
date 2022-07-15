905c3796a9830eebaa2f44ffc1079898c157cd64
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2022-07-15 16:48:01 +0800

    Create Car.java

---
+++ test/car/Car.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.awt.*;
+
+import static graphics.Graphics.*;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        createWindow("Demo");
+
+        // draw a car
+
+        createPath();
+
+        moveTo(60, 120);
+        lineTo(80, 120);
+        quadTo(90, 140, 100, 120);
+        lineTo(160, 120);
+        quadTo(170, 140, 180, 120);
+        lineTo(200, 120);
+        curveTo(195, 100, 200, 80, 160, 80);
+        lineTo(110, 80);
+        lineTo(90, 100);
+        lineTo(60, 100);
+        lineTo(60, 120);
+
+        closePath();
+        drawPath();
+        
+    }
+
+}
